4f296e268c420e398009ccf6fa8cf65800957cdf
UMRzcz-831 <2488289735@qq.com>
2022-04-19 21:30:37 +0800

    feat: layout，主题色

--- packages/renderer/src/App.vue
+++ packages/renderer/src/App.vue
@@ -1,25 +1,34 @@
 <template>
-	<NConfigProvider :theme="darkTheme">
-		<NMessageProvider>
-			<router-view></router-view>
-		</NMessageProvider>
-	</NConfigProvider>
+  <NConfigProvider :theme="darkTheme" :theme-overrides="themeOverrides">
+    <NMessageProvider>
+      <router-view></router-view>
+    </NMessageProvider>
+  </NConfigProvider>
 </template>
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import { NConfigProvider, darkTheme, NMessageProvider } from 'naive-ui'
+import {
+  NConfigProvider,
+  darkTheme,
+  NMessageProvider,
+  GlobalThemeOverrides,
+} from 'naive-ui';
+
+const themeOverrides: GlobalThemeOverrides = {
+//   common: {
+//     primaryColor: '#2c3e50',
+//   },
+     Layout: {
+	   color: '#222',
+	   headerColor:'#222',
+	   siderColor: '#222'
+	   
+	 },
+	 
+
+};
 </script>
 <style>
-html,
-body {
-	margin: 0;
-	padding: 0;
-	background: #222;
-}
-.logo-box {
-	display: flex;
-	width: 100%;
-	justify-content: center;
-}
+
 </style>
--- packages/renderer/src/layout/layout.vue
+++ packages/renderer/src/layout/layout.vue
@@ -1,18 +1,30 @@
 <template>
   <div id="main-page">
-    <n-layout has-sider>
-      <n-layout-sider>
-        <SideMenu />
-      </n-layout-sider>
-      <n-layout-header>777</n-layout-header>
-      <div class="handle-bar"></div>
-      <n-layout-content>
-        <router-view />
-      </n-layout-content>
-    </n-layout>
+    <NLayout class="main-layout">
+      <!-- <NLayoutHeader><div class="handle-bar">999</div></NLayoutHeader> -->
+      <NLayout has-sider>
+        <NLayoutSider :width="150" bordered>
+          <LoginStatus />
+          <SideMenu />
+        </NLayoutSider>
+        <NLayoutContent>
+          <router-view />
+        </NLayoutContent>
+      </NLayout>
+    </NLayout>
   </div>
 </template>
 <script setup lang="ts">
+import { NLayout, NLayoutContent, NLayoutSider, NLayoutHeader } from 'naive-ui';
 import SideMenu from '../components/SideMenu/index.vue';
+import LoginStatus from '../components/LoginStatus';
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.main-layout {
+  min-height: 100vh;
+}
+.n-layout-sider {
+  height: 100vh;
+}
+
+</style>
